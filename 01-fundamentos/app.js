4a8e7a4fd0ef9217f4d594486449dc81dfd8cf6b
Raúl Hidalgo Gaspar <rhidalgog@izzi.mx>
2026-06-16 17:50:42 -0600

    01 Fundamentos: introducción a la consola

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,13 +1,13 @@
 // console.log('Hola Mundo');
 
 // Variables
-let a = 10; // Esta variable puede ser reasignada
-var b = 10; // Esta variable también puede ser reasignada, pero tiene un alcance diferente al de 'let'
-const c = 10; // Esta variable no puede ser reasignada, es constante
+let a1 = 10; // Esta variable puede ser reasignada
+var b1 = 10; // Esta variable también puede ser reasignada, pero tiene un alcance diferente al de 'let'
+const c1 = 10; // Esta variable no puede ser reasignada, es constante
 
-// c = 20; // Esto generará un error porque 'c' es una constante
-a = 20; // Esto es válido, 'a' puede ser reasignada
-b = 20; // Esto también es válido, 'b' puede ser reasignada
+// c1 = 20; // Esto generará un error porque 'c1' es una constante
+a1 = 20; // Esto es válido, 'a1' puede ser reasignada
+b1 = 20; // Esto también es válido, 'b1' puede ser reasignada
 
 // Diferencias entre var, let y const
 function ejemploVar() {
@@ -35,3 +35,18 @@ function ejemploLet() {
 let a = 10, b = 10; // Se pueden declarar múltiples variables en una sola línea
 let c = a + b; // c es igual a 20
 console.log(c); // Imprime 20
+
+// Tipos de console
+console.log('Esto es un mensaje de log');
+console.warn('Esto es una advertencia');
+console.error('Esto es un mensaje de error');
+
+console.log('El valor de a es:', a); // Imprime el valor de a
+console.log('El valor de b es:', b); // Imprime el valor de b
+
+console.log({a}); // Imprime el valor de a como un objeto, lo que facilita su lectura en la consola
+console.log({b}); // Imprime el valor de b como un objeto
+
+console.log('%c Esto es un mensaje con estilo', 'color: blue; font-size: 20px;'); // Imprime un mensaje con estilo personalizado
+
+console.table([10, 20, "holas"]); // Imprime una tabla con los valores del array, lo que facilita su lectura en la consola
